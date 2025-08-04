513c2c3809c005f19cd353ba4e7d641302ba0250
Barasa2023 <officialbarasa@gmail.com>
2025-08-04 14:32:48 +0300

    Feat: Add search button

--- weather/views.py
+++ weather/views.py
@@ -5,7 +5,7 @@ import requests
 def index(request):
     """Render the index page."""
     api_key = '9750ce5bd3642197183bf37924b84db9'
-    city = request.GET.get('city', 'Mombasa')
+    city = request.GET.get('city')
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     data = response.json()
