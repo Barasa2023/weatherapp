902dc202d1c6833dd73602b6166811fbc80497e5
Barasa2023 <officialbarasa@gmail.com>
2025-08-04 13:29:07 +0300

    Feat: Add url file

---
+++ weather/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'weather'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('forecast/', views.forecast, name='forecast'),
+    path('about/', views.about, name='about'),
+]
