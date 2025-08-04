512297a256dec2a5113186614b8806fcf62fcc7a
Barasa2023 <officialbarasa@gmail.com>
2025-08-04 14:18:12 +0300

    Fix: Update context data keys

--- weather/views.py
+++ weather/views.py
@@ -5,16 +5,14 @@ import requests
 def index(request):
     """Render the index page."""
     api_key = '9750ce5bd3642197183bf37924b84db9'
-    city = request.GET.get('city', 'Nairobi')
-
-    url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}'
+    city = request.GET.get('city', 'Mombasa')
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     data = response.json()
 
-    context = {
-        'location': data['location']['name'],
-        'temperature': data['current']['temp_c'],
-        'condition': data['current']['condition']['text'],
+    weather = {
+        'location': city,
+        'temperature': data['main']['temp'],
+        'condition': data['weather'][0]['description'],
     }
-
-    return render(request, 'weather/index.html', context)
+    return render(request, 'weather/index.html', {'weather': weather})
