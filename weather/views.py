bd18d3d65bed0858f49afcd02028a988196f3924
Barasa2023 <officialbarasa@gmail.com>
2025-08-04 14:46:01 +0300

    Feat: Dynamic icons

--- weather/views.py
+++ weather/views.py
@@ -5,7 +5,7 @@ import requests
 def index(request):
     """Render the index page."""
     api_key = '9750ce5bd3642197183bf37924b84db9'
-    city = request.GET.get('city')
+    city = request.GET.get('city', 'Mombasa')
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     data = response.json()
@@ -14,5 +14,7 @@ def index(request):
         'location': city,
         'temperature': data['main']['temp'],
         'condition': data['weather'][0]['description'],
+        'humidity': data['main']['humidity'],
+        'wind': data['wind']['speed'],
     }
     return render(request, 'weather/index.html', {'weather': weather})
