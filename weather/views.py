444c02fe0751e30553ad4caf6c7bd04d188d23c8
Barasa2023 <officialbarasa@gmail.com>
2025-08-04 13:48:16 +0300

    Feat: Index View

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def index(request):
+    """Render the index page."""
+    api_key = '9750ce5bd3642197183bf37924b84db9'
+    city = request.GET.get('city', 'Nairobi')
+
+    url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}'
+    response = requests.get(url)
+    data = response.json()
+
+    context = {
+        'location': data['location']['name'],
+        'temperature': data['current']['temp_c'],
+        'condition': data['current']['condition']['text'],
+    }
+
+    return render(request, 'weather/index.html', context)
