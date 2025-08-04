6755c2f0441aaff41a3085818c2f67b9ea06a614
Barasa2023 <officialbarasa@gmail.com>
2025-08-04 15:23:10 +0300

    Feat: Add weather forecast

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 import requests
 
 # Create your views here.
@@ -9,12 +10,32 @@ def index(request):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     data = response.json()
+    weather, forecast = {}, []
 
-    weather = {
-        'location': city,
-        'temperature': data['main']['temp'],
-        'condition': data['weather'][0]['description'],
-        'humidity': data['main']['humidity'],
-        'wind': data['wind']['speed'],
-    }
-    return render(request, 'weather/index.html', {'weather': weather})
+    if data.get('main'):
+        weather = {
+            'location': city,
+            'temperature': data['main']['temp'],
+            'condition': data['weather'][0]['description'],
+            'humidity': data['main']['humidity'],
+            'wind': data['wind']['speed'],
+        }
+
+    
+    #Weather forecast for the next 5 days
+    forecast_url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric"
+    forecast_response = requests.get(forecast_url)
+    forecast_data = forecast_response.json()
+
+    if forecast_data.get('list'):
+        for entry in forecast_data['list']:
+            dt = datetime.fromtimestamp(entry['dt'])
+            if dt.hour == 12:  # Get forecast for noon
+                forecast.append({
+                    'date': dt.strftime('%Y-%m-%d'),
+                    'temperature': entry['main']['temp'],
+                    'desc': entry['weather'][0]['description'],
+                    'icon': entry['weather'][0]['icon'],
+                })
+
+    return render(request, 'weather/index.html', {'weather': weather, 'forecast': forecast})
